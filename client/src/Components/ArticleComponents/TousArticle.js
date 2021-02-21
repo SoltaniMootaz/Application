55fc7e40890a350caedbab318e4a6a638331061a
SoltaniMootaz <mootaz.1.soltani2019@gmail.com>
2021-02-21 13:07:41 +0100

    patch 0.1.2

---
+++ client/src/Components/ArticleComponents/TousArticle.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Row } from 'react-bootstrap'
+import Axios from 'axios'
+
+const urlcat="http://localhost:3001/api/afficherCategorie";
+const urlart="http://localhost:3001/api/afficherArticles&";
+
+function TousArticle() {
+    return (
+        <>
+           <Row>
+           </Row> 
+        </>
+    )
+}
+
+export default TousArticle
