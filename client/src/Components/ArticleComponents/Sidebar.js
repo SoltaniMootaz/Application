c75171d69746e3daea60ad858b22919d5e3bdb9f
hmejbri <hmejbri431@gmail.com>
2021-02-21 13:08:07 +0100

    patch 0.1.2

--- client/src/Components/ArticleComponents/Sidebar.js
+++ client/src/Components/ArticleComponents/Sidebar.js
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../css/Article.css'
-import{Container, Nav, Row, Col, Navbar, Form, FormControl, Button} from 'react-bootstrap'
+import{Nav, Button} from 'react-bootstrap'
+import AjouterCat from './ajouterCategorie'
+
 function Sidebar() {
+    const [state, setState] = useState({
+        isOpen: Boolean(false)
+    })
+
     return (
+        <>
         <Nav className="col-3 d-none d-md-block sidebar"
         activeKey="/"
         style={{backgroundColor:'#00A600'}}
         >
-            <div className="sidebar-sticky"></div>
-            <div className="s-link">
-        <Nav.Item >
-            <Nav.Link href="/Article" style={{color:'white'}}><Button variant="outline" style={{borderRadius:"1.5em",borderColor:"white",color:'white'}}>+</Button> Ajouter article</Nav.Link>
-        </Nav.Item>
-        <Nav.Item > 
-            <Nav.Link href="/Article" style={{color:'white'}}><Button variant="outline" style={{borderRadius:"1.5em",borderColor:"white",color:'white'}}>+</Button> Ajouter catégorie</Nav.Link>
-        </Nav.Item>
+        <div className="sidebar-sticky"></div>
+        <div className="s-link">
+            <Nav.Item >
+                <Nav.Link  style={{color:'white'}}><Button onClick={() => setState({ isOpen: true })} variant="outline" style={{borderRadius:"1.5em",borderColor:"white",color:'white'}}>+</Button> Ajouter article</Nav.Link>
+            </Nav.Item>
+            <Nav.Item > 
+                <Nav.Link href="/Article" style={{color:'white'}}><Button variant="outline" style={{borderRadius:"1.5em",borderColor:"white",color:'white'}}>+</Button> Ajouter catégorie</Nav.Link>
+            </Nav.Item>
         </div>
         </Nav>
+            <AjouterCat 
+                handleOpen={state.isOpen}
+                handleClose={() => setState({ isOpen: false })}
+            />
+        </>
     )
 }
 
