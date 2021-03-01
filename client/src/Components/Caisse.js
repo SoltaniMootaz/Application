9a8e78fe832c0ccc62c11aa5125b670e795c0355
Mootaz <mootaz.1.soltani2019@gmail.com>
2021-03-01 11:09:07 +0100

    0.1.3

--- client/src/Components/Caisse.js
+++ client/src/Components/Caisse.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import Ticket from './CaisseComponents/ticket'
+import {
+    Container,
+    Row,
+    Col,
+    Spinner,
+    Navbar,
+    Form,
+    FormControl,
+  } from "react-bootstrap";
 
 function Caisse() {
     return (
-        <Ticket></Ticket>
+        <Container fluid>
+         <Row>
+         <Col>
+          
+         </Col>  
+
+         <Col>
+         
+         </Col>
+         </Row>
+        </Container>
+
     )
 }
 
