11eccde6fbc783ad9780e69231581d4d52a0ddbb
Mootaz <mootaz.1.soltani2019@gmail.com>
2021-03-02 01:32:07 +0100

    0.1.2

--- client/src/Components/Article.js
+++ client/src/Components/Article.js
@@ -1,163 +1,121 @@
-import React, {useState, useEffect} from 'react'
-import './css/Article.css'
-import{Container,  Row, Col, Spinner,Navbar,Form,FormControl,Nav,AccordionToggle} from 'react-bootstrap'
-import Navbarup from './ArticleComponents/Navbarup.js'
-import Sidebar from './ArticleComponents/Sidebar.js'
-import Axios from 'axios'
-import TousArticle from './ArticleComponents/TousArticle.js'
-import ArticlesChercher from './ArticleComponents/ArticleChercher.js'
-import AjouterCat from "./ArticleComponents/ajouterCategorie";
-import AjouterArt from "./ArticleComponents/ajouterArticle";
+import React, { useState, useEffect } from "react";
+import "./css/Article.css";
+import {
+  Container,
+  Row,
+  Col,
+  Spinner,
+  Navbar,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import Navbarup from "./ArticleComponents/Navbarup.js";
+import Sidebar from "./ArticleComponents/Sidebar.js";
+import Axios from "axios";
+import TousArticle from "./ArticleComponents/TousArticle.js";
+import ArticlesChercher from "./ArticleComponents/ArticleChercher.js";
+
 function Article() {
-    const urlcat="http://localhost:3001/api/afficherCategorie";
-    const urlart="http://localhost:3001/api/afficherArticles&";
-    var isLoading=true;
- 
+  const urlcat = "http://localhost:3001/api/afficherCategorie";
+  const urlart = "http://localhost:3001/api/afficherArticles&";
+  var isLoading = true;
 
-    const [dataCat,setDataCat]=useState([]);
-    const [dataArt,setDataArt]=useState([]);
-    const [isLoading1, setLoading1] = useState(true);
-    const [isLoading2, setLoading2] = useState(true);
-    const [isSearching,setIsSearching]=useState(false);
-    const [value,setValue]=useState();
+  const [dataCat, setDataCat] = useState([]);
+  const [dataArt, setDataArt] = useState([]);
+  const [isLoading1, setLoading1] = useState(true);
+  const [isLoading2, setLoading2] = useState(true);
+  const [isSearching, setIsSearching] = useState(false);
+  const [value, setValue] = useState();
 
-    const [state, setState] = useState({
-        isOpen: Boolean(false),
-      });
-    
-      const [state1, setState1] = useState({
-        isOpen: Boolean(false),
-      });
-
-    const getCategories=()=>{
+  const getCategories = () => {
     Axios.get(urlcat)
-        .then(res => setDataCat(res.data))
-        .catch(err=>console.log(err))
-    setLoading1(false)
-}
+      .then((res) => setDataCat(res.data))
+      .catch((err) => console.log(err));
+    setLoading1(false);
+  };
 
+  const getArticles = () => {
+    Axios.get(urlart)
+      .then((res) => setDataArt(res.data))
+      .catch((err) => console.log(err));
+    setLoading2(false);
+  };
 
-   const getArticles=()=>{
-   Axios.get(urlart)
-   .then(res => setDataArt(res.data))
-   .catch(err=>console.log(err))
-   setLoading2(false)
-}
+  useEffect(() => {
+    getCategories();
+    getArticles();
+  }, []);
 
+  const handleSearch = (e) => {
+    if (e.target.value === "") {
+      setIsSearching(false);
+      setValue(e.target.value);
+    } else {
+      setValue(e.target.value);
+      setIsSearching(true);
+    }
+  };
 
-    useEffect(() => {
-        getCategories();
-        getArticles();
-      
-                 }, [])
-
-                 const handleSearch=(e)=>{
-                     if (e.target.value===""){
-                
-                      setIsSearching(false);
-                      setValue(e.target.value)
-                    }
-                    else {
-                    setValue(e.target.value)
-                    setIsSearching(true);
-                    
-                    }
-                }
-            
-        
-     if(isLoading1&&isLoading2){
-     return (<Spinner animation="border" role="status">
-     <span className="sr-only">Loading...</span>
-   </Spinner>);
-
-      }else{     
-      isLoading=false;
-      
+  if (isLoading1 && isLoading2) {
     return (
-        <>
-     <Container fluid>
-                <Row>
-                <Col  xs={12} sm={12} md={9} xl={9} id="page-content-wrapper" className="nopadding" > 
-                    <Row>  <Col  xs={12} sm={12} md={12} xl={12} id="page-content-wrapper">
-                    <Navbar className=" justify-content-center border-bottom " expand="lg" style={{  boxShadow:'inset -1px 0 0 rgba(0, 0, 0, .1)',}}>
-                    <Navbarup/>
-                    <Nav.Item className="navItem">
-                        <Form className="searchForm" inline>
-                        <FormControl type="text" placeholder="Search" onChange={handleSearch} className="searchBarr" style={{
-                            marginRight:'3em',
-                            borderLeft: 0,
-                            borderTop: 0,
-                            borderRight: 0,
-                            borderColor: "#176cd4",
-                            borderRadius: "0em",
-                        }}/>
-                        </Form>
-                        </Nav.Item>
-                        
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                        <Nav.Item className="navItem">
-                        <Nav.Link
-                        style={{ textAlign:'center' }}
-                        onClick={() => setState1({ isOpen: true })}
-                        >
-                        Ajouter article
-                        </Nav.Link >
-                        </Nav.Item>
-                        <Nav.Item className="navItem">
-                        <Nav.Link
-                        style={{textAlign:'center'  }}
-                        onClick={() => setState({ isOpen: true })}
-                        >
-                        Ajouter catégorie
-                        </Nav.Link>
-                        </Nav.Item>
-                        </Nav>
-                        </Navbar.Collapse>    
-                        <Nav.Item>
-                        <Form className="searchForm1" inline>
-                        <FormControl type="text" placeholder="Search" onChange={handleSearch} className="searchBar1" style={{
-                            marginRight:'3em', 
-                            borderLeft: 0,
-                            borderTop: 0,
-                            borderRight: 0,
-                            borderColor: "#176cd4",
-                            borderRadius: "0em",
-                        }}/>
-                        </Form>
-                        </Nav.Item>                 
-                        </Navbar>
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  } else {
+    isLoading = false;
 
-                        
-                     
-      
-
-                        </Col> </Row>
+    return (
+      <>
+        <Container fluid>
+          <Row>
+            <Col xs={9} id="page-content-wrapper" className="nopadding">
+              <Row>
+                {" "}
+                <Col xs={12} id="page-content-wrapper">
+                  <Navbar
+                    className="bg-light justify-content-between"
+                    expand="lg"
+                    style={{ boxShadow: "inset -1px 0 0 rgba(0, 0, 0, .1)" }}
+                  >
+                    <Navbarup />
+                    <div style={{ alignContent: "flex-end", float: "left" }}>
+                      <Form inline>
+                        <FormControl
+                          type="text"
+                          placeholder="Search"
+                          className="mr-sm-2"
+                          onChange={handleSearch}
+                          style={{ borderRadius: "0.8em" }}
+                        />
+                      </Form>
+                    </div>
+                  </Navbar>
+                </Col>{" "}
+              </Row>
+                  {!isSearching ? (
                     <Row>
-                        <Col style={{marginTop:'5%',marginLeft:'5%'}} >
-                            { !isSearching ?
-                            <TousArticle dataCat={dataCat} dataArt={dataArt}  isLoading={isLoading}></TousArticle>
-                            :
-                            <ArticlesChercher value={value}  chercherDans={dataArt} /> }
-                        </Col>
-                    </Row>
-                     </Col>
-                    <Col   sm={3} md={3} xl={3}  id="sidebar-wrapper" className="nopadding" >
-                    <Sidebar />
+                    <Col style={{ marginTop: "5%", marginLeft: "5%" }}>
+                    <TousArticle
+                      dataCat={dataCat}
+                      dataArt={dataArt}
+                      isLoading={isLoading}
+                    ></TousArticle>
                     </Col>
-                </Row>
-    </Container>
-    <AjouterCat
-        handleOpen={state.isOpen}
-        handleClose={() => setState({ isOpen: false })}
-        />
-        <AjouterArt
-        handleOpen={state1.isOpen}
-        handleClose={() => setState1({ isOpen: false })}
-      />
-    </>
-    );}
+                    </Row>
+                  ) : (
+                    <ArticlesChercher value={value} chercherDans={dataArt} />
+                  )}
+                
+            </Col>
+            <Col xs={3} id="sidebar-wrapper" className="nopadding">
+              <Sidebar />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
-export default Article
+export default Article;
--- client/src/Components/Caisse.js
+++ client/src/Components/Caisse.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import Ticket from './CaisseComponents/ticket'
 import {
     Container,
@@ -8,23 +8,97 @@ import {
     Navbar,
     Form,
     FormControl,
+    Nav
   } from "react-bootstrap";
-
+  import { AiFillHome } from "react-icons/ai";
+  import Axios from "axios";
 function Caisse() {
+  const urlart = "http://localhost:3001/api/afficherArticles&";
+  const [dataArt, setDataArt] = useState([]);
+  const [isLoading2, setLoading2] = useState(true);
+
+  const getArticles = () => {
+    Axios.get(urlart)
+      .then((res) => setDataArt(res.data))
+      .catch((err) => console.log(err));
+    setLoading2(false);
+  };
+
+  useEffect(() => {
+
+    getArticles();
+  }, []);
+
+
+  if (isLoading2) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  } else {
+     console.log(dataArt)
     return (
         <Container fluid>
-         <Row>
-         <Col>
-          
-         </Col>  
-
-         <Col>
-         
-         </Col>
-         </Row>
+            <Row>       
+                <Col  md={4} xl={4}  id="sidebar-wrapper" className="nopadding border-right" > 
+                <Ticket></Ticket>
+                </Col>
+                <Col xs={12} sm={12} md={8} xl={8} id="page-content-wrapper" className="nopadding">
+                    <Row> 
+                    <Col  xs={12} sm={12} md={12} xl={12} id="page-content-wrapper">
+                        <Navbar className=" justify-content-center border-bottom border-left" expand="lg" style={{  boxShadow:'inset -1px 0 0 rgba(0, 0, 0, .1)',}}>
+                        <Navbar.Brand href="#home"><AiFillHome  className="icon" style={{width:'1.7em',height:'1.7em'}}/></Navbar.Brand>
+                        <Nav className="mr-auto " >
+                        <Nav.Link href=""  ><p className="homeBtn" >Home</p></Nav.Link>
+                        </Nav>
+                        <Nav.Item className="navItem">
+                        <Form className="searchForm" inline>
+                        <FormControl type="text" placeholder="Search"  className="searchBarr" style={{
+                                marginRight:'3em',
+                                borderLeft: 0,
+                                borderTop: 0,
+                                borderRight: 0,
+                                borderColor: "#176cd4",
+                                borderRadius: "0em" }}/>
+                        </Form>
+                        </Nav.Item>                               
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                        <Nav.Item className="navItem">
+                        <Nav.Link
+                        style={{ textAlign:'center' }} >
+                            Ajouter article
+                        </Nav.Link >
+                        </Nav.Item>
+                        <Nav.Item className="navItem">
+                        <Nav.Link
+                        style={{textAlign:'center'  }}>
+                            Ajouter catégorie
+                        </Nav.Link>
+                        </Nav.Item>
+                        </Nav>
+                        </Navbar.Collapse>    
+                        <Nav.Item>
+                        <Form className="searchForm1" inline>
+                        <FormControl type="text" placeholder="Search" className="searchBar1" style={{
+                                marginRight:'3em', 
+                                borderLeft: 0,
+                                borderTop: 0,
+                                borderRight: 0,
+                                borderColor: "#176cd4",
+                                borderRadius: "0em"}}/>
+                            </Form>
+                            </Nav.Item>                 
+                        </Navbar>
+                     </Col>
+                    </Row>
+                  </Col> 
+                </Row>
         </Container>
 
     )
-}
+}}
 
 export default Caisse; 
