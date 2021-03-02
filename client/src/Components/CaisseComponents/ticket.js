fd35e1fd201b2d73d4f00187f0303d6183f07b9c
Mootaz <mootaz.1.soltani2019@gmail.com>
2021-03-02 01:22:34 +0100

    0.1.2

--- client/src/Components/CaisseComponents/ticket.js
+++ client/src/Components/CaisseComponents/ticket.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import Vente from './vente'
-import {Button } from "react-bootstrap";
+import {Button,Nav } from "react-bootstrap";
 
 
 function Ticket() {
@@ -10,12 +10,20 @@ function Ticket() {
 
     return (
         <>
-        <Button variant="primary" onClick={() => setState({ isOpen: true })}>Vendre</Button>
+       
+      <h4 style={{paddingTop:'2.05em'}}><center>Ticket</center></h4>
+        <hr />
+     
+         <Nav className="justify-content-start navbar fixed-bottom navbar-light border-top">   
+          <Button variant="primary" onClick={() => setState({ isOpen: true })}>Vendre</Button>
+          
+        </Nav>
 
         <Vente
         handleOpen={state.isOpen}
         handleClose={() => setState({ isOpen: false })}
         />
+    
       </>
     )
 }
