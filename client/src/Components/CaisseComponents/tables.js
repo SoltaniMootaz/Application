76abb8b9db10d3e3d5ff9f5709d3176aebe8cb4c
hmejbri <hmejbri431@gmail.com>
2021-03-23 21:06:29 +0100

    ....

--- client/src/Components/CaisseComponents/tables.js
+++ client/src/Components/CaisseComponents/tables.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-function tables() {
-    return (
-        <div>
-            
-        </div>
-    )
+////////////////////////////////////////////////////////
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+function Table(props) {
+
+    const submit = (e) => {
+        e.preventDefault();
+    }
+
+  return (
+    <>
+    <Dialog fullWidth={true} onClose={props.handleClose} aria-labelledby="customized-dialog-title" open={props.handleOpen}>
+        <DialogTitle id="customized-dialog-title" onClose={props.handleClose}>
+          Tables
+        </DialogTitle>
+        <DialogContent dividers>
+          
+        </DialogContent>
+        <DialogActions style={{justifyContent:'center'}}>
+            <Button variant="contained" color="primary">
+                Valider
+            </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
 
-export default tables
+export default Table;
